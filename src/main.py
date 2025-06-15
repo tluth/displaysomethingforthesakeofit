e1e34621aab73d6d3c2f40e9ac059eab710d6d6b
tluth <tom.lutherborrow@gmail.com>
2025-06-15 02:54:38 +0200

    add co2 brick

--- src/main.py
+++ src/main.py
@@ -3,33 +3,40 @@ import time
 from sensors import get_humidity_temperature_readings
 from display import lcd_init
 
-MASTERBRICK_HOST = "192.168.1.4"
+MASTERBRICK_HOST = "192.168.1.9"
 MASTERBRICK_PORT = 4223
 HUMIDITY_UID = "C1g"
 TEMP_UID = "zEG"
+CO2_UID = "Gtr"
+
 
 def main() -> None:
     try:
         lcd = lcd_init()
-        
+
         while True:
-            sensor_data = get_humidity_temperature_readings(MASTERBRICK_HOST, MASTERBRICK_PORT, HUMIDITY_UID, TEMP_UID)
+            sensor_data = get_humidity_temperature_readings(
+                MASTERBRICK_HOST, MASTERBRICK_PORT, HUMIDITY_UID, TEMP_UID, CO2_UID
+            )
             # Format display text
-            temp_text = f"Temp: {sensor_data['temperature']:.1f}C"
-            hum_text = f"Hum: {sensor_data['humidity']:.1f}%"
+            temp_text = f"{sensor_data['temperature']:.1f}C | "
+            hum_text = f"{sensor_data['humidity']:.1f}% hum"
+            co2_text = f"CO2: {sensor_data["co2"]} ppm"
 
             # Print to LCD
             lcd.cursor_pos = (0, 0)
             lcd.write_string(temp_text)
-            lcd.cursor_pos = (1, 0)
+            lcd.cursor_pos = (0, 8)
             lcd.write_string(hum_text)
+            lcd.cursor_pos = (1, 0)
+            lcd.write_string(co2_text)
             time.sleep(2)  # Update every 2 seconds
 
     except KeyboardInterrupt:
         print("Stopping readings and clearing LCD.")
         time.sleep(2)
         lcd.clear()
-    
+
     finally:
         lcd.clear()
         exit()
--- src/sensors.py
+++ src/sensors.py
@@ -3,30 +3,35 @@ import time
 from tinkerforge.ip_connection import IPConnection
 from tinkerforge.bricklet_humidity import BrickletHumidity
 from tinkerforge.bricklet_temperature import BrickletTemperature
+from tinkerforge.bricklet_co2 import BrickletCO2
 
 
-def get_humidity_temperature_readings(host: str, port: int, humidity_uid: str, temp_uid: str) -> dict:
+def get_humidity_temperature_readings(
+        host: str, port: int, humidity_uid: str, temp_uid: str, co2_uid: str
+) -> dict:
     """Fetch Humidity and Temperature readings and display them on the LCD."""
     ipcon = IPConnection()
     humidity = BrickletHumidity(humidity_uid, ipcon)
     temperature = BrickletTemperature(temp_uid, ipcon)
+    co2 = BrickletCO2(co2_uid, ipcon)
 
     try:
         ipcon.connect(host, port)
 
         humidity_value = humidity.get_humidity() / 10.0
         temperature_value = temperature.get_temperature() / 100.0
-
+        co2_value = co2.get_co2_concentration()
         print("Connected to Masterbrick...")
+
         return {
             "temperature": temperature_value,
-            "humidity": humidity_value
+            "humidity": humidity_value,
+            "co2": co2_value
         }
 
     except Exception:
         print("Error reading sensors")
-    
+
     finally:
         ipcon.disconnect()
         print("Disconnected.")
-
